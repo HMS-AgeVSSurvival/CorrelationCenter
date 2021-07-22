e1c9d238f94d16fdfc39193e70c7bb4326b85aff
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2021-07-22 12:21:12 -0400

    Merge dumps with data

--- residual/compute_residual.py
+++ residual/compute_residual.py
@@ -1,5 +1,6 @@
 import argparse
 import sys
+import os
 
 import pandas as pd
 
@@ -8,7 +9,7 @@ from residual import AGE_COLUMN, GENDER_COLUMN
 
 def residual_cli(argvs=sys.argv[1:]):
     parser = argparse.ArgumentParser(
-        "Pipeline to compute the residual from the predictions"
+        "Pipeline to merge the dumps with the data and to compute the residual from the predictions"
     )
     parser.add_argument(
         "-mc",
@@ -21,11 +22,36 @@ def residual_cli(argvs=sys.argv[1:]):
     args = parser.parse_args(argvs)
     print(args)
 
-    compute_residual(args.main_category, args.category)
+    data = merge_data_dumps(args.main_category, args.category)
+    compute_residual(data, args.main_category, args.category)
 
 
-def compute_residual(main_category, category):
+def merge_data_dumps(main_category, category):
     data = pd.read_feather(f"../TrainingCenter/data/{main_category}/{category}.feather").set_index("SEQN")
+
+    for target in ["age", "all", "cvd", "cancer"]:
+        for algorithm in ["elastic_net", "light_gbm"]:
+            for random_state in [1, 2]:
+                path_dump = f"../TrainingCenter/dumps/prediction/{target}/{main_category}/{category}/{algorithm}_{random_state}.feather"
+                
+                if os.path.exists(path_dump):
+                    dump = pd.read_feather(path_dump).set_index("SEQN")
+                    data.loc[dump.index, dump.columns] = dump
+                
+                if os.path.exists(path_dump.replace("prediction", "feature_importances")):
+                    dump = pd.read_feather(path_dump.replace("prediction", "feature_importances")).set_index("index").T   
+                    for dump_index in dump.index:
+                        data.loc[dump_index] = dump.loc[dump_index]
+            
+            if os.path.exists(path_dump.replace("prediction", "feature_importances").replace("2", "train")):
+                dump = pd.read_feather(path_dump.replace("prediction", "feature_importances").replace("2", "train")).set_index("index").T
+                for dump_index in dump.index:
+                    data.loc[dump_index] = dump.loc[dump_index]
+
+    return data
+
+
+def compute_residual(data, main_category, category):
     raw_data = data.copy()
 
     data.drop(columns=data.columns[~(data.columns.str.startswith("prediction") | data.columns.isin([AGE_COLUMN, GENDER_COLUMN]))], inplace=True)
@@ -40,4 +66,5 @@ def compute_residual(main_category, category):
         residual.columns = residual.columns.map(lambda prediction_name: prediction_name.replace("prediction", "residual"))
         raw_data.loc[residual.index, residual.columns] = residual
 
+    raw_data.index = raw_data.index.astype(str, copy=False)
     raw_data.reset_index().to_feather(f"data/{main_category}/{category}.feather")
--- residual/shell_script/run_residual.sh
+++ residual/shell_script/run_residual.sh
@@ -2,12 +2,6 @@
 
 for MAIN_CATEGORY in "examination" "laboratory" "questionnaire"
 do
-    for PATH_CATEGORY in ../TrainingCenter/data/$MAIN_CATEGORY/*
-    do
-        IFS='/' read -r a a a a FILE_CATEGORY <<<"$PATH_CATEGORY"
-        CATEGORY=$(echo $FILE_CATEGORY | cut -d "." -f 1)
-
-        rm out/residual/$MAIN_CATEGORY/$CATEGORY.out
-        sbatch -J residual/$MAIN_CATEGORY/$CATEGORY -o out/residual/$MAIN_CATEGORY/$CATEGORY.out residual/shell_script/unit_residual.sh -mc $MAIN_CATEGORY -c $CATEGORY
-    done
+    [ ! -e out/residual/$MAIN_CATEGORY.out ] || rm out/residual/$MAIN_CATEGORY.out
+    sbatch -J residual/$MAIN_CATEGORY -o out/residual/$MAIN_CATEGORY.out residual/shell_script/unit_residual.sh -mc $MAIN_CATEGORY
 done
