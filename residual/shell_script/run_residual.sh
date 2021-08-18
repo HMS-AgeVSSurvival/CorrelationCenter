8e240902307ef5743ffe76967f0428046ba44b7c
Théo VINCENT <theo.vincent@eleves.enpc.fr>
2021-08-18 03:53:36 -0400

    Clean residual

--- residual/shell_script/run_residual.sh
+++ residual/shell_script/run_residual.sh
@@ -3,5 +3,6 @@
 for MAIN_CATEGORY in "examination" "laboratory" "questionnaire"
 do
     echo -n > out/residual/$MAIN_CATEGORY.out
-    sbatch -J residual/$MAIN_CATEGORY -o out/residual/$MAIN_CATEGORY.out residual/shell_script/unit_residual.sh -mc $MAIN_CATEGORY
+    [ ! -e error/residual/$MAIN_CATEGORY.out ] || rm error/residual/$MAIN_CATEGORY.out
+    sbatch -J residual/$MAIN_CATEGORY -e error/residual/$MAIN_CATEGORY.out -o out/residual/$MAIN_CATEGORY.out residual/shell_script/unit_residual.sh -mc $MAIN_CATEGORY
 done
